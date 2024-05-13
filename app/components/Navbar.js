5e3309dcc9d88191d485642d954d5f6c1fc1540c
Allan <79191644+AllanDroter@users.noreply.github.com>
2024-05-13 01:30:05 -0400

    more accidental nav work in main switching back to my branch now

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -5,34 +5,34 @@ import logomain from '../Assets/main_logo_banner.png'
 
 export default function Navbar() {
   return (
-    <nav style={{ display: 'flex', justifyContent: 'flex-end', backgroundColor: '#F3E3C2' }}>
+    <nav className='header'>
     <div style={{ marginRight: 'auto' }}>
         <Link href="/">
           <Image src={logomain} alt="Logo" width={355} height={52} />
         </Link>
       </div>
-      <ul style={{ display: 'flex', listStyle: 'none', margin: 0, padding: 0 }}>
-        <li style={{ marginLeft: '10px' }}>
+      <ul>
+        <li>
           <Link className='linker' href="/">
             Home
           </Link>
         </li>
-        <li style={{ marginLeft: '10px' }}>
+        <li>
           <Link className='linker' href="/search">
             Search
           </Link>
         </li>
-        <li style={{ marginLeft: '10px' }}>
+        <li>
           <Link className='linker' href="/login">
             Login
           </Link>
         </li>
-        <li style={{ marginLeft: '10px' }}>
+        <li>
           <Link className='linker' href="/profile">
             Profile
           </Link>
         </li>
-        <li style={{ marginLeft: '10px' }}>
+        <li>
           <Link className='linker' href="/about">
             About Us
           </Link>
