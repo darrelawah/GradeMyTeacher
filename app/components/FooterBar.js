cd572d8b0acbabf17f5094420246466cd5190a14
Allan <79191644+AllanDroter@users.noreply.github.com>
2024-05-13 00:14:37 -0400

    quick modifications to footer etc

--- app/components/FooterBar.js
+++ app/components/FooterBar.js
@@ -4,7 +4,7 @@ export default function FooterBar() {
   return (
     <div className='footer'>
       <div>
-        <p>
+        <p className='footerText'>
           We are in no way associated with Rate My Professors as an entity or business rival. This site is a university project made by a collaborative effort on part on the students in our About section during the spring semester of 2024.
         </p>
       </div>
